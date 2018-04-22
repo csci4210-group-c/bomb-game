c1e37a54f7f50b74563847b4feada55a58397488
aisha <aishaalandijani@gmail.com>
2018-04-22 09:15:51 -0500

    Unit Testing

---
+++ app/src/test/java/com/csci4210/bombgame/BomberTest.java
@@ -0,0 +1,15 @@
+package com.csci4210.bombgame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Pr.Game on 4/22/2018.
+ */
+public class BomberTest {
+    @Test
+    public void isOnFireTile() throws Exception {
+    }
+
+}
